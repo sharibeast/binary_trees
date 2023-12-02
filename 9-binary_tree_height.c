fa1f9d34a7b48a1f29cfd10b274d7c1e277dd1b9
sharibeast <georginasharif@gmail.com>
2023-12-02 03:41:21 +0300

    fix binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,7 +17,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	left_node_length = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	right_node_length = tree->right ? 1 +  binary_tree_height(tree->right) : 0;
-	return ((left_node_length > right_node_length) ? left_node_length : right_node_length);
+	return ((left_node_length > right_node_length) ?
+	 left_node_length : right_node_length);
 
 	return (0);
 }
