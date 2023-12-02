654481f5a00c6cb6daa9c675d71e3ca41bd04dca
sharibeast <georginasharif@gmail.com>
2023-12-02 03:55:48 +0300

    Adding header file

--- binary_trees.h
+++ binary_trees.h
@@ -25,6 +25,7 @@ typedef struct binary_tree_s binary_tree_t;
 
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
